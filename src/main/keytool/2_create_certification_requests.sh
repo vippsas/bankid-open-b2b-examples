98800f6ae5f751b60d977c536c96ab094d2385a1
Håvard Kvam Buvik <hkbuvik@gmail.com>
2016-12-06 13:41:03 +0100

    OB2B-3, OB2B-13, OB2B-14: Add signature algorithm parameter to certification request

--- src/main/keytool/2_create_certification_requests.sh
+++ src/main/keytool/2_create_certification_requests.sh
@@ -2,5 +2,5 @@
 
 source 0_set_environment_variables.sh
 
-${JAVA_HOME}/keytool -certreq -alias authkey -file "${AUTH_CSR}" -keystore "${KEY_STORE}" -storepass ${STORE_PASSWORD}
-${JAVA_HOME}/keytool -certreq -alias signkey -file "${SIGN_CSR}" -keystore "${KEY_STORE}" -storepass ${STORE_PASSWORD}
+${JAVA_HOME}/keytool -certreq -sigalg SHA256withRSA -alias authkey -file "${AUTH_CSR}" -keystore "${KEY_STORE}" -storepass ${STORE_PASSWORD} -v
+${JAVA_HOME}/keytool -certreq -sigalg SHA256withRSA -alias signkey -file "${SIGN_CSR}" -keystore "${KEY_STORE}" -storepass ${STORE_PASSWORD} -v
--- src/main/keytool/4_import_certificate_chain.sh
+++ src/main/keytool/4_import_certificate_chain.sh
@@ -2,5 +2,5 @@
 
 source 0_set_environment_variables.sh
 
-${JAVA_HOME}/keytool -import -trustcacerts -file "${AUTH_PKCS7}" -keystore "${KEY_STORE}" -storepass ${STORE_PASSWORD} -alias authkey
-${JAVA_HOME}/keytool -import -trustcacerts -file "${SIGN_PKCS7}" -keystore "${KEY_STORE}" -storepass ${STORE_PASSWORD} -alias signkey
+${JAVA_HOME}/keytool -import -trustcacerts -file "${AUTH_PKCS7}" -keystore "${KEY_STORE}" -storepass ${STORE_PASSWORD} -alias authkey -v
+${JAVA_HOME}/keytool -import -trustcacerts -file "${SIGN_PKCS7}" -keystore "${KEY_STORE}" -storepass ${STORE_PASSWORD} -alias signkey -v
