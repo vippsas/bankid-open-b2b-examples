8192dffac33464c3a3366dfcd8666edf8a09b3e6
svesol <svein.otto.solem@kantega.no>
2016-12-06 08:29:27 +0100

    OB2B-2: refaktorisert s.a. man nå kan signere ocsp requesten med noe annet enn man har signert meldingen med.

--- src/main/java/no/bankid/openb2b/OcspRequester.java
+++ src/main/java/no/bankid/openb2b/OcspRequester.java
@@ -1,19 +1,21 @@
 package no.bankid.openb2b;
 
 
-import org.bouncycastle.asn1.*;
+import org.bouncycastle.asn1.ASN1ObjectIdentifier;
+import org.bouncycastle.asn1.ASN1Sequence;
+import org.bouncycastle.asn1.DEROctetString;
+import org.bouncycastle.asn1.DERTaggedObject;
 import org.bouncycastle.asn1.ocsp.OCSPObjectIdentifiers;
 import org.bouncycastle.asn1.ocsp.OCSPResponse;
-import org.bouncycastle.asn1.x500.X500Name;
-import org.bouncycastle.asn1.x509.*;
 import org.bouncycastle.asn1.x509.Extension;
+import org.bouncycastle.asn1.x509.Extensions;
+import org.bouncycastle.asn1.x509.GeneralName;
+import org.bouncycastle.asn1.x509.X509ObjectIdentifiers;
 import org.bouncycastle.cert.X509CertificateHolder;
 import org.bouncycastle.cert.jcajce.JcaX509CertificateHolder;
 import org.bouncycastle.cert.ocsp.*;
 import org.bouncycastle.operator.DefaultDigestAlgorithmIdentifierFinder;
-import org.bouncycastle.operator.DefaultSignatureAlgorithmIdentifierFinder;
 import org.bouncycastle.operator.OperatorCreationException;
-import org.bouncycastle.operator.bc.BcRSAContentSignerBuilder;
 import org.bouncycastle.operator.bc.BcRSAContentVerifierProviderBuilder;
 import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
 import org.bouncycastle.operator.jcajce.JcaDigestCalculatorProviderBuilder;
@@ -24,13 +26,13 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.lang.reflect.Array;
 import java.math.BigInteger;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.security.PrivateKey;
-import java.security.cert.*;
 import java.security.cert.Certificate;
+import java.security.cert.CertificateEncodingException;
+import java.security.cert.X509Certificate;
 import java.util.*;
 
 import static no.bankid.openb2b.OcspChecker.BEGIN_CERTIFICATE;
@@ -40,60 +42,58 @@ public class OcspRequester {
     private final static Logger LOGGER = LoggerFactory.getLogger(OcspRequester.class);
     private static final int CONNECT_TIMEOUT_MS = 15000;
 
-
-    public static class OCSPResponseHolder {
-        final String signerCert;
-        final String signerName;
-        final byte[] rawOcspResponse;
-
-        public OCSPResponseHolder(String signerCert, String signerName, byte[] rawOcspResponse) {
-            this.signerCert = signerCert;
-            this.signerName = signerName;
-            this.rawOcspResponse = rawOcspResponse;
-        }
-    }
-
     /**
-     * @param certToBeValidated must contain the authorityInfoAccessExtension holding the url for the va service
+     * @param certToBeValidated must contain the authorityInfoAccessExtension holding the url for the va service and the certificate serialnumber
      * @param issuerCert        issuer of certToBeValidated, used together with certToBeValidated's serialnumber
      * @param signerChain       used for signing the ocsp request to the va service, used if nonempty
+     * @param signerKey         used together with the signerchain to sign the ocsp request, required if signerchain is nonempty
      * @return DEREncoded bytes
-     * @optional key used together with the signerchain to sign the ocsp request, used if signerchain is nonempty
      */
-    OCSPResponseHolder sendOcspRequestGetResponse(X509Certificate certToBeValidated, X509Certificate issuerCert,
-                                                  List<? extends java.security.cert.Certificate> signerChain, Optional<PrivateKey> signerKey) {
+    public byte[] sendOcspRequestGetResponse(X509Certificate certToBeValidated, X509Certificate issuerCert,
+                                             List<? extends java.security.cert.Certificate> signerChain, PrivateKey signerKey) {
 
         final URL ocspUrlFromCertificate = getOcspUrlFromCertificate(certToBeValidated);
         final OCSPReq ocspReq;
         BigInteger nonce = BigInteger.valueOf(System.currentTimeMillis());
         try {
-            CertificateID id = new CertificateID(new JcaDigestCalculatorProviderBuilder().build().get(CertificateID.HASH_SHA1),
-                    new JcaX509CertificateHolder(issuerCert), certToBeValidated.getSerialNumber());
+            CertificateID id =
+                    new CertificateID(new JcaDigestCalculatorProviderBuilder().build().get(CertificateID.HASH_SHA1),
+                            new JcaX509CertificateHolder(issuerCert), certToBeValidated.getSerialNumber());
 
             LOGGER.debug("Connecting to instance.toString() = " + ocspUrlFromCertificate);
 
-            OCSPReqBuilder gen = new OCSPReqBuilder();
-            gen.addRequest(id);
+            OCSPReqBuilder ocspReqBuilder = new OCSPReqBuilder();
+            ocspReqBuilder.addRequest(id);
 
             // create details for nonce extension
             Extension ocspNonce = new Extension(OCSPObjectIdentifiers.id_pkix_ocsp_nonce, false, nonce.toByteArray());
-            gen.setRequestExtensions(new Extensions(ocspNonce));
+            ocspReqBuilder.setRequestExtensions(new Extensions(ocspNonce));
 
             if (signerChain != null && !signerChain.isEmpty()) {
-                final List<X509CertificateHolder> x509CertificateHolders = toHolder(signerChain);
-                gen.setRequestorName(x509CertificateHolders.get(0).getSubject());
-                ocspReq = gen.build(new JcaContentSignerBuilder("SHA256WithRSA").build(signerKey.get()), x509CertificateHolders.toArray(new X509CertificateHolder[signerChain.size()]));
+                final List<X509CertificateHolder> x509CertificateHolders = toCertificateHolder(signerChain);
+                ocspReqBuilder.setRequestorName(x509CertificateHolders.get(0).getSubject());
+                ocspReq = ocspReqBuilder.build(new JcaContentSignerBuilder("SHA256WithRSA").build(signerKey),
+                        x509CertificateHolders.toArray(new X509CertificateHolder[signerChain.size()]));
             } else {
-                ocspReq = gen.build();
+                ocspReq = ocspReqBuilder.build();
             }
 
         } catch (OCSPException | CertificateEncodingException | OperatorCreationException e) {
             throw new IllegalArgumentException(e);
         } // TODO: se http://www.programcreek.com/java-api-examples/index.php?api=org.bouncycastle.ocsp.OCSPResp for enklere kode, og husk å legge inn sjekk på nonceverdien sent over.
-        final byte[] ocspReqBytes;
+        byte ocspResponseBytes[] = sendRequest(ocspUrlFromCertificate, ocspReq);
+
+        checkResult(ocspResponseBytes, nonce);
+
+        return ocspResponseBytes;
+    }
+
+    private byte[] sendRequest(URL ocspUrlFromCertificate, OCSPReq ocspReq) {
         try {
-            ocspReqBytes = ocspReq.getEncoded();
-            System.out.println("OcspReq:\n" + new String(Base64.getMimeEncoder().encode(ocspReqBytes)));
+            byte[] ocspReqBytes = ocspReq.getEncoded();
+            if (LOGGER.isDebugEnabled()) {
+                LOGGER.debug("Sending OcspReq:\n" + new String(Base64.getMimeEncoder().encode(ocspReqBytes)));
+            }
             HttpURLConnection vaConnection = (HttpURLConnection) ocspUrlFromCertificate.openConnection();
             vaConnection.setConnectTimeout(CONNECT_TIMEOUT_MS);
             vaConnection.setReadTimeout(CONNECT_TIMEOUT_MS);
@@ -129,11 +129,13 @@ public class OcspRequester {
                         }
 
                         ocspResponseBytes = Arrays.copyOf(ocspResponseBytes, nReadSoFar);
-                        LOGGER.debug("OcspResp:\n" + new String(Base64.getMimeEncoder().encode(ocspResponseBytes)));
+                        if (LOGGER.isDebugEnabled()) {
+                            LOGGER.debug("Received OcspResp:\n" + new String(Base64.getMimeEncoder().encode(ocspResponseBytes)));
+                        }
                         break;
                     }
-                    final ResponseSignerCertAndName responseSignerCertAndName = showResponse(ocspResponseBytes, nonce);
-                    return new OCSPResponseHolder(responseSignerCertAndName.responseSignerCert, responseSignerCertAndName.responseSignerName, ocspResponseBytes);
+
+                    return ocspResponseBytes;
                 }
             }
         } catch (IOException e) {
@@ -141,7 +143,7 @@ public class OcspRequester {
         }
     }
 
-    private List<X509CertificateHolder> toHolder(List<? extends Certificate> signerChain) {
+    private List<X509CertificateHolder> toCertificateHolder(List<? extends Certificate> signerChain) {
         List<X509CertificateHolder> ret = new ArrayList<>();
         for (Certificate c : signerChain) {
             try {
@@ -153,51 +155,38 @@ public class OcspRequester {
         return ret;
     }
 
-    private static class ResponseSignerCertAndName {
-        final String responseSignerCert;
-        final String responseSignerName;
-
-        ResponseSignerCertAndName(String responseSignerCert, String responseSignerName) {
-            this.responseSignerCert = responseSignerCert;
-            this.responseSignerName = responseSignerName;
-        }
-    }
-
-    private ResponseSignerCertAndName showResponse(byte[] ocspResponseBytes, BigInteger expectedNonceValue) {
+    private void checkResult(byte[] ocspResponseBytes, BigInteger expectedNonceValue) {
         try {
 
             final OCSPResp ocspResp = new OCSPResp(OCSPResponse.getInstance(ocspResponseBytes));
             if (ocspResp.getStatus() != 0) {
-                throw new IOException("Invalid OCSP status " + ocspResp.getStatus());
+                throw new IllegalStateException("Invalid OCSP status " + ocspResp.getStatus());
             }
             BasicOCSPResp basicOCSPResp = (BasicOCSPResp) ocspResp.getResponseObject();
+            final Optional<Extension> nonceExtension = Optional.ofNullable(basicOCSPResp.getExtension(OCSPObjectIdentifiers.id_pkix_ocsp_nonce));
+            final BigInteger foundNonce = nonceExtension.map(nonceReceived -> new BigInteger(nonceReceived.getExtnValue().getOctets())).orElse(null);
 
-            final BigInteger foundNonce = new BigInteger(basicOCSPResp.getExtension(OCSPObjectIdentifiers.id_pkix_ocsp_nonce).getExtnValue().getOctets()); // TODO: nullchecks
-            if (Objects.equals(foundNonce, expectedNonceValue)) {
-                System.out.println("Nonce as expected.");
+            if (!Objects.equals(foundNonce, expectedNonceValue)) {
+                throw new IllegalStateException("Invalid nonce value in OCSP response expected: " + expectedNonceValue + " received:" + foundNonce);
             }
-            final X500Name responseSignerName = basicOCSPResp.getResponderId().toASN1Primitive().getName();
-            System.out.println("Responder name is " + responseSignerName);
-            final X509CertificateHolder[] certs = basicOCSPResp.getCerts();
-            final boolean signatureValid = basicOCSPResp.isSignatureValid(
-                    new BcRSAContentVerifierProviderBuilder(new DefaultDigestAlgorithmIdentifierFinder()).build(certs[0]));
-            System.out.println("Response signature is valid: " + signatureValid);
-            //X509Certificate signerCert = new JcaX509CertificateConverter().getCertificate(certs[0]);
-            System.out.println("certificate used for signing OCSP response is: ");
-            final String responseSignerCertMimeEncoded = BEGIN_CERTIFICATE + new String(Base64.getMimeEncoder().encode(certs[0].getEncoded())) + "\n" + END_CERTIFICATE;
-            System.out.println(responseSignerCertMimeEncoded);
+            if (LOGGER.isDebugEnabled()) {
+                LOGGER.debug("OCSP Responder's name is " + basicOCSPResp.getResponderId().toASN1Primitive().getName());
+                final X509CertificateHolder[] certs = basicOCSPResp.getCerts();
+                final boolean signatureValid = basicOCSPResp.isSignatureValid(new BcRSAContentVerifierProviderBuilder(new DefaultDigestAlgorithmIdentifierFinder()).build(certs[0]));
+                LOGGER.debug("OCSP Responder's signature is valid: " + signatureValid);
+                LOGGER.debug("OCSP Responder's certificate used for signing OCSP response is:\n " + BEGIN_CERTIFICATE + new String(Base64.getMimeEncoder().encode(certs[0].getEncoded())) + "\n" + END_CERTIFICATE);
 
-            SingleResp singleResp = basicOCSPResp.getResponses()[0];
+                SingleResp singleResp = basicOCSPResp.getResponses()[0];
 
-            final CertificateStatus certStatus = singleResp.getCertStatus();
-            if (certStatus instanceof RevokedStatus) {
-                System.out.println("Cert revoked");
-            } else if (certStatus == CertificateStatus.GOOD) {
-                System.out.println("Certstatus is good");
-            } else {
-                System.out.println("Certstatus is unknown");
+                final CertificateStatus certStatus = singleResp.getCertStatus();
+                if (certStatus instanceof RevokedStatus) {
+                    LOGGER.debug("Certificate status is REVOKED");
+                } else if (certStatus == CertificateStatus.GOOD) {
+                    LOGGER.debug("Certificate status is GOOD");
+                } else {
+                    LOGGER.debug("Certificate status is UNKNOWN");
+                }
             }
-            return new ResponseSignerCertAndName(responseSignerCertMimeEncoded, responseSignerName.toString());
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
